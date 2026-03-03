3a83d9468070ebc6319510c868b2772d3000ccc3
joan.santos <joan.santos@redeflex.com.br>
2026-03-03 08:44:15 -0300

    first commit

---
+++ src/main/java/com/nexus/nexus_list/NexusListApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.nexus_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexus/nexus_list/NexusListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.nexus_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
